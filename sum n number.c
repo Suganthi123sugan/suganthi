cc0e47378c231b3a63ca5ef62f36c70891d95b44
Suganthi123sugan <48052955+Suganthi123sugan@users.noreply.github.com>
2019-03-25 16:02:24 +0530

    Create sum n number.c

---
+++ sum n number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,c=0;
+    printf("enter the number");
+    scanf("%d",&a);
+    for(int i=1;i<=a;i++)
+    {
+        c +=i;
+    }
+    printf("c of=%d",c);
+    
+    
+}
