4e2918df311db848fdb601d84ba1850ffeb00454
Suganthi123sugan <48052955+Suganthi123sugan@users.noreply.github.com>
2019-03-28 03:56:35 -0700

    Create 5th table.c

---
+++ 5th table.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,a;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        a=5*i;
+        printf(" %d",a);
+    }
+    
+}
